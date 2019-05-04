a8875028cb92fc31c73fbafa7314f776638e5e03
Abdul <awsayed@alaska.edu>
2019-05-04 23:52:04 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,5 +1,19 @@
-def get_first_name_of_season_winner(data, season)
-  # code here
+def get_first_name_of_season_winner(data, season_number)
+
+	winner = ""
+  data.each { |season, contestants|
+
+		if (season.to_s == season_number)
+			contestants.each { |contestant|
+
+				if (contestant[:status] == "Winner")
+					winner = contestant[:name]
+				end
+			}
+		end
+  }
+
+	return winner.split(" ")[0]
 end
 
 def get_contestant_name(data, occupation)
