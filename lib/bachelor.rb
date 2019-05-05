b93f3afc30a4f1cf39f1fc9f79ebc8f9ca8d3b63
Abdul <awsayed@alaska.edu>
2019-05-05 01:09:49 +0000

    Done.

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,19 +1,83 @@
-def get_first_name_of_season_winner(data, season)
-  # code here
+def get_first_name_of_season_winner(data, season_number)
+
+	winner = ""
+  data.each { |season, contestants|
+
+		if (season.to_s == season_number)
+			contestants.each { |contestant|
+
+				if (contestant["status"] == "Winner")
+					winner = contestant["name"]
+				end
+			}
+		end
+  }
+
+	return winner.split(" ")[0]
 end
+
+###############################################################
 
 def get_contestant_name(data, occupation)
-  # code here
+  
+	data.each { |season, contestants|
+		contestants.each { |contestant|
+			if (contestant["occupation"] == occupation)
+				return contestant["name"]
+			end 
+		}
+	}
+
 end
+
+################################################################
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+
+	counter = 0
+
+	data.each { |season, contestants|
+		contestants.each { |contestant|
+			if (contestant["hometown"] == hometown)
+				counter += 1
+			end 
+		}
+	}
+	return counter
 end
+
+################################################################
 
 def get_occupation(data, hometown)
-  # code here
+
+	data.each { |season, contestants|
+		contestants.each { |contestant|
+			if (contestant["hometown"] == hometown)
+				return contestant["occupation"]
+			end 
+		}
+	}
+
 end
 
-def get_average_age_for_season(data, season)
-  # code here
+###############################################################
+
+def get_average_age_for_season(data, season_number)
+  
+	ages = []
+
+  data.each { |season, contestants|
+
+		if (season == season_number)
+			contestants.each { |contestant|
+				ages << contestant["age"]
+			}
+		end
+  }
+	sum = 0
+	for age in ages
+		sum += age.to_f
+	end
+
+	return (sum / ages.length).round
 end
