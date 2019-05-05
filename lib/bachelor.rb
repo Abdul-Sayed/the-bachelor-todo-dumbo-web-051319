c8b9fa1980f870416752cf92b23215469765fae7
Abdul <awsayed@alaska.edu>
2019-05-05 00:26:22 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -16,17 +16,25 @@ def get_first_name_of_season_winner(data, season_number)
 	return winner.split(" ")[0]
 end
 
+###############################################################
+
 def get_contestant_name(data, occupation)
   # code here
 end
+
+################################################################
 
 def count_contestants_by_hometown(data, hometown)
   # code here
 end
 
+################################################################
+
 def get_occupation(data, hometown)
   # code here
 end
+
+###############################################################
 
 def get_average_age_for_season(data, season)
   # code here
